ea4f494db48c9844a1fac3458fc579a8b2521266
mbpro15-717 <cgriggs01@github.com>
2017-11-14 14:26:31 -0800

    move region

--- loadbalancer/config.tf
+++ loadbalancer/config.tf
@@ -4,10 +4,10 @@ module "mini_module" {
   name         = "group1-lb"
   service_port = 80
   target_tags  = []
+  region       = "us-west1"
 
   firewall_project = ""
   network          = "default"
   project          = "Module Testing"
-  region           = "us-west1"
   session_affinity = "NONE"
 }
