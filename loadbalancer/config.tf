bb0081e9bd13cf546a5ca94554d8ef6022e7cf0f
mbpro15-717 <cgriggs01@github.com>
2017-11-14 14:21:03 -0800

    add source path

--- loadbalancer/config.tf
+++ loadbalancer/config.tf
@@ -1,15 +1,13 @@
-
-
 module "mini_module" {
+  source = "GoogleCloudPlatform/lb/google"
 
-	name 		= "group1-lb"
-	service_port	= 80
-	target_tags	= [  ]
+  name         = "group1-lb"
+  service_port = 80
+  target_tags  = []
 
-	firewall_project= ""
-	network		= "default"
-	project		= "Module Testing"
-	region		= "us-west1"
-	session_affinity= "NONE"
-
+  firewall_project = ""
+  network          = "default"
+  project          = "Module Testing"
+  region           = "us-west1"
+  session_affinity = "NONE"
 }
